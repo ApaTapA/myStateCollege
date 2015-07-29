4ba0feed3f703480d1585e881ba80afd1832abf9
Thomas Diffendal <thomasdiffendal@Thomass-MacBook-Pro.local>
2015-07-28 20:12:35 -0400

    Initial Commit

---
+++ myStateCollege/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myStateCollege
+//
+//  Created by Thomas Diffendal on 7/28/15.
+//  Copyright (c) 2015 ApaTapA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
